42d776ca715d424fe4b4ed22917a9fb81cf7956f
Ryan McCormick <rmccorm4@binghamton.edu>
2019-08-03 15:47:45 -0700

    refactor to handle empty string or name

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return "Hello, " + name
 }
 
 func main() {
 	fmt.Println(hello("Ryan"))
+	fmt.Println(hello(""))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello("Ryan")
-	want := "Hello, Ryan"
+	t.Run("Saying hello with a given name", func(t *testing.T) {
+		got := hello("Ryan")
+		want := "Hello, Ryan"
 
-	if got != want {
-		t.Errorf("got %q - want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q - want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, World' when an empty string given", func(t *testing.T) {
+		got := hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q - want %q", got, want)
+		}
+	})
 }
