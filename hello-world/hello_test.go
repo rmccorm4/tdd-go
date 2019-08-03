7cf7b61fc32ac2bab2b10a9fb7ce6d8efe392bf4
Ryan McCormick <rmccorm4@binghamton.edu>
2019-08-03 15:50:46 -0700

    refactor to add re-usable assertion for less code

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,21 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("Saying hello with a given name", func(t *testing.T) {
-		got := hello("Ryan")
-		want := "Hello, Ryan"
-
+	assertion := func(t *testing.T, got, want string) {
+		// Error gives line number of calling function rather than this function
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q - want %q", got, want)
 		}
+	}
+	t.Run("Saying hello with a given name", func(t *testing.T) {
+		got := hello("Ryan")
+		want := "Hello, Ryan"
+		assertion(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when an empty string given", func(t *testing.T) {
 		got := hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q - want %q", got, want)
-		}
+		assertion(t, got, want)
 	})
 }
